6d5dba742e3678bb71e41750f9061b51a2ba212f
Randeep Singh <rand-singh@users.noreply.github.com>
2021-02-11 22:00:03 +0000

    Create basic form for todos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css';
+import Input from './components/Input'
 
 function App() {
   return (
     <div className="app">
-      <h1>To Do App</h1>      
+      <Input />
     </div>
   )
 }
---
+++ src/components/Input.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import './Input.css'
+
+const Input = () => {
+    const [input, setInput] = useState('')
+
+    const addTodo = () => {
+        console.log('add todo')
+    }
+
+    return (
+        <div className="input">
+            <input type="text" value={input} onChange={e=>setInput(e.target.value)}/>
+            <button onClick={addTodo}>Add!</button>
+        </div>
+    )
+}
+
+export default Input
